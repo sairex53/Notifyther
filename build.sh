75d6d94a7e8e7f9900a483977a8acdc17c228cf5
SAIREX <113220566+sairex53@users.noreply.github.com>
2024-02-12 15:59:20 +0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,1 @@
+cd ~/Desktop/ ; git clone https://github.com/sairex53/Notifyther.git; cd Notifyther ; brew install python3 ; python3 -m pip install --upgrade pip ; pip3 install -r requirements.txt ; pip3 install py2app ; python3 setup.py py2app 
